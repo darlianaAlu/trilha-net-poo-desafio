cfe5fb5dc2549113352b1cf29a037372e9c4e3cf
Darliana <darlianacampos@gmail.com>
2025-05-20 11:35:54 -0400

    Modificacoes em classe e instanciamento

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "9912345", modelo: "Modelo 1 ", imei:"111111", memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("/n");
+
+Console.WriteLine("Smatphone Iphone");
+Smartphone iphone = new Iphone(numero: "9987456", modelo: "Modelo 17", imei: "222222", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
